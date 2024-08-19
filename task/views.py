bf6ef09a7a0b7ab180d4a81d94e59c645b7aac8f
Hossein Amirabdollahi <hosseinamirabdollahi8@gmail.com>
2024-08-19 16:51:00 +0330

    security updated

--- task/views.py
+++ task/views.py
@@ -9,9 +9,33 @@ tasks = ["foo", "bar", "baz"]
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
+# def index(request):
+#     return render(request, "tasks/index.html", {
+#         "tasks": tasks
+#     })
+
+# def add(request):
+#     if request.method == "POST":
+#         form = NewTaskForm(request.POST)
+#         if form.is_valid():
+#             task = form.cleaned_data["task"]
+#             tasks.append(task)
+#             return HttpResponseRedirect(reverse("tasks:index"))
+#         else:
+#             return render(request, "tasks/add.html", {
+#                 "form": form
+#             })
+
+#     return render(request, "tasks/add.html", {
+#         "form": NewTaskForm()
+#     })
+
+
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
@@ -19,7 +43,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
